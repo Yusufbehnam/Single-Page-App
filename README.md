3a35e72a8c2c9b572bea062c22e4b9b5a47c58aa
Yusuf Behnam Seviş <yusufbehnams@gmail.com>
2023-08-11 15:04:13 +0300

    Verilerin Fetch API ile Getirilmesi

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 ## SPA
 
 Bir js dosyasının o sayfada yüklenmesi ve herşeyin onun üzerinden yüklenmesi ve gitmesi anlamına gelir.
+
+Aşağıdaki siteden hash tag ile ilgili bilgi alabiiliriz.
+
+https://developer.mozilla.org/en-US/docs/Web/API/Location
+
+
+
+
--- main.js
+++ main.js
@@ -5,18 +5,27 @@ console.log("window.location.hostname",window.location.hostname)
 
 const hashLinks = document.querySelectorAll(".hash-route-link")
 
+const app =document.querySelector("#app")
+
 const routes = {
     "" : {
-        title : "Ana Sayfa"
-    },
+        title : "Ana Sayfa",
+        isApi:false,
+        data:"./home.html"},
     "#todos" : {
-        title : "Todo List"
+        title : "Todo List",
+        isApi:true,
+        data:"https://jsonplaceholder.typicode.com/todos/"
     },
     "#about" : {
-        title : "About Us"
+        title : "About Us",
+        data:"./about-us.html",
+        isApi:false
     },
     "#contact" : {
-        title : "Contact"
+        title : "Contact",
+        data:"./contact.html",
+        isApi:false
     }
 
 }
@@ -24,6 +33,22 @@ const routes = {
 function checkRoute (hash = window.location.hash){
     // console.log(routes[hash])
     document.title = routes[hash].title
+    const dataUrl =routes[hash].data
+    isApi=routes[hash].isApi
+    if (isApi) {
+        app.innerHTML=""
+        const crateUl = document.createElement("ul")
+        app.append(crateUl)
+        fetch(dataUrl).then(res=>res.json()).then(data =>
+            data.forEach(item => {
+                const crateLi = document.createElement("li")
+                crateLi.innerHTML=item.title
+                crateUl.append(crateLi)
+                
+            }) )
+    } else if (!isApi) {
+    fetch(dataUrl).then(res=>res.text()).then(data => app.innerHTML=data)
+}
 }
 
 hashLinks.forEach(item => item.addEventListener("click", (event)=>{
